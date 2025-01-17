1fb0f56f25f7f8f007fd8de3e4f5d1d938560054
dengbaole <dengbaole155@gmail.com>
2025-01-17 22:59:08 +0800

    信号量

--- main/task_handle.c
+++ main/task_handle.c
@@ -1,33 +1,23 @@
 #include "task_handle.h"
 
 
-QueueHandle_t queue_handle = NULL;
-
-
-typedef struct {
-    uint16_t value;
-}queue_data_t;
-
+SemaphoreHandle_t bin_sem;
 
 void task_handle(void* pvParameters) { 
-	//从队列里面接收数据并打印
-	queue_data_t queue_data;
+	//释放信号量
 	while(1) {
-		if(pdTRUE == xQueueReceive(queue_handle, &queue_data, 100)) {
-			ESP_LOGI("queuee", "task_handle: %d", queue_data.value);
-		}
+		xSemaphoreGive(bin_sem);
+		vTaskDelay(pdMS_TO_TICKS(1000));
 	}
 }
 
 
 void task_handle2(void* pvParameters) {
-	//向队列里面发送数据并打印
-	queue_data_t queue_data;
-	memset(&queue_data, 0, sizeof(queue_data_t));
+	//等待信号量
 	while(1) {
-		xQueueSend(queue_handle, &queue_data, 100);
-		vTaskDelay(pdMS_TO_TICKS(1000));
-		queue_data.value++;
+		if(pdTRUE == xSemaphoreTake(bin_sem, portMAX_DELAY)) {
+			ESP_LOGI("bin", "Semaphore taken");
+		}
 	}
 }
 
@@ -35,7 +25,7 @@ void task_handle2(void* pvParameters) {
 
 
 void task_init(void) {
-	queue_handle = xQueueCreate(10, sizeof(queue_data_t));
+	bin_sem = xSemaphoreCreateBinary();
 	xTaskCreatePinnedToCore(task_handle, "task_handle", 2048, NULL, 10, NULL, 1);
 	xTaskCreatePinnedToCore(task_handle2, "task_handle2", 2048, NULL, 10, NULL, 1);
 }
