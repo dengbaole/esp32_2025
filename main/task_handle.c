4ca09365382fa7601a4062c69971799599a3ff30
dengbaole <dengbaole155@gmail.com>
2025-05-26 23:10:49 +0800

    同一个函数创建多个任务

--- main/task_handle.c
+++ main/task_handle.c
@@ -1,25 +1,20 @@
 #include "task_handle.h"
 
-void task_handle(void* pvParameters) {
-	while(1) {
-		ESP_LOGI("main", "task_handle");
-		vTaskDelay(pdMS_TO_TICKS(1000));
-	}
+void task_function(void* pvParameters) {
+    // 从参数中获取任务ID和延迟时间
+    int task_id = ((int*)pvParameters)[0];
+    int delay_ms = ((int*)pvParameters)[1];
+    
+    while(1) {
+        ESP_LOGI("main", "Task %d running", task_id);
+        vTaskDelay(pdMS_TO_TICKS(delay_ms));
+    }
 }
-
-
-void task_handle2(void* pvParameters) {
-	while(1) {
-		ESP_LOGI("main", "task_handle2");
-		vTaskDelay(pdMS_TO_TICKS(1000));
-	}
-}
-
-
-
 
 void task_init(void) {
-	xTaskCreatePinnedToCore(task_handle, "task_handle", 2048, NULL, 10, NULL, 1);
-	xTaskCreatePinnedToCore(task_handle2, "task_handle2", 2048, NULL, 10, NULL, 1);
+    static int task1_params[2] = {1, 500};
+    static int task2_params[2] = {2, 1000};
+    
+    xTaskCreatePinnedToCore(task_function, "task_1", 2048, (void*)task1_params, 10, NULL, 1);
+    xTaskCreatePinnedToCore(task_function, "task_2", 2048, (void*)task2_params, 10, NULL, 1);
 }
-
